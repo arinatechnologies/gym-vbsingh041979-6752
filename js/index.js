3f86e543cd5600560836750296f1234be036590b
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 23:29:33 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,10 @@
+{
+    "hero": "images/index1.jpg",
+    "showcase": "images/index2.jpg",
+    "features": [
+        "images/feature1.jpg",
+        "images/feature2.jpg",
+        "images/feature3.jpg",
+        "images/feature4.jpg"
+    ]
+}
