590776399b9d838e0c015a6b3fba1ca4fe489482
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 23:29:50 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,6 @@
+{
+  "hero_image": "modern gym interior with high-end equipment",
+  "team_member_placeholder": "fitness trainer portrait",
+  "mission_values_background": "group of diverse people working out together",
+  "cta_background": "happy people exercising in gym"
+}
